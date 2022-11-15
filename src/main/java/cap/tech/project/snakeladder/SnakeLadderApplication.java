22aff912a8f70cba9ddd9b41e67765d9fdfeb9a4
Satyajeet Prakash <sant8430@gmail.com>
2022-11-15 18:43:21 +0530

    initial commit

---
+++ src/main/java/cap/tech/project/snakeladder/SnakeLadderApplication.java
@@ -0,0 +1,13 @@
+package cap.tech.project.snakeladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeLadderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeLadderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cap/tech/project/snakeladder/SnakeLadderApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.tech.project.snakeladder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeLadderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
